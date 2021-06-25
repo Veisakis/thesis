737616513b22d3a0c5eba4738e01bd36e96189f7
Veisakis <manosveisakis@gmail.com>
2021-06-25 13:22:49 +0300

    Hydro Battery

---
+++ problems.py
@@ -0,0 +1,25 @@
+from scipy.optimize import minimize
+
+
+def objective(x):
+    return -x[0] * x[1]
+
+
+def constraint(x):
+    return x[0] + x[1] - 8
+
+
+bnds = ((None, None), (None, None))
+cons = {'type': 'eq', 'fun': constraint}
+
+x0 = (2, 2)
+
+res = minimize(
+    objective,
+    x0,
+    method='SLSQP',
+    bounds=bnds,
+    constraints=cons
+)
+
+print(res)
