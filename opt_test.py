1d01f72d935e36efa337205c0549d64da2d20079
Veisakis <74550377+Veisakis@users.noreply.github.com>
2021-09-22 17:09:58 +0300

    Update opt_test.py

--- opt_test.py
+++ opt_test.py
@@ -1,8 +1,6 @@
 from scipy.optimize import minimize
 
 objective = lambda x: x[0]*x[3]*(x[0]+x[1]+x[2])+x[2]
-
-
 constraint1 = lambda x: x[0]*x[1]*x[2]*x[3]-25.0
 
 def constraint2(x):
@@ -11,9 +9,7 @@ def constraint2(x):
         sum_sq -= x[i]**2
     return sum_sq
 
-
 x0 = [1, 5, 5, 1]
-
 b = (1,5)
 bnds = (b,b,b,b)
 
@@ -21,7 +17,5 @@ con1 = {'type':'ineq', 'fun': constraint1}
 con2 = {'type': 'eq', 'fun': constraint2}
 cons = [con1, con2]
 
-
 sol = minimize(objective, x0, method='SLSQP', bounds=bnds, constraints=cons)
 print(sol)
-
